1aa4e3f2daa8bcf493025b9813ac8fd7189dba16
Alexey Manukhin <axxapy@gmail.com>
2016-08-09 14:59:36 -0700

    insert & update commands protos

--- commands.go
+++ commands.go
@@ -18,11 +18,24 @@ type hs_cmd_find struct {
 	offset  int
 }
 
+type cmd_insert struct {
+	command string
+	params  []string
+}
+
+type cmd_update struct {
+	command  string
+	criteria []string
+	limit    int
+	offset   int
+	mop      string
+	newvals  []string
+}
+
 func (f *cmd_openindex) write(w io.Writer) error {
 	if _, err := fmt.Fprintf(w, "%s\t%s\n", f.command, strings.Join(f.params, "\t")); err != nil {
 		return err
 	}
-
 	return nil
 }
 
@@ -30,6 +43,19 @@ func (f *hs_cmd_find) write(w io.Writer) error {
 	if _, err := fmt.Fprintf(w, "%s\t%s\t%d\t%d\n", f.command, strings.Join(f.params, "\t"), f.limit, f.offset); err != nil {
 		return err
 	}
+	return nil
+}
 
+func (f *cmd_insert) write(w io.Writer) error {
+	if _, err := fmt.Fprintf(w, "%s\t%s\n", f.command, strings.Join(f.params, "\t")); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *cmd_update) write(w io.Writer) error {
+	if _, err := fmt.Fprintf(w, "%s\t%s\t%d\t%d\t%s\t%s\n", f.command, strings.Join(f.criteria, "\t"), f.limit, f.offset, f.mop, strings.Join(f.newvals, "\t")); err != nil {
+		return err
+	}
 	return nil
 }
